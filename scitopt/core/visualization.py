e2fddc3b57087dd8f8567774922028a272082714
Kevin-Tofu <koheitech001@gmail.com>
2025-04-05 00:06:17 +0900

    remove cause of warning

--- scitopt/core/visualization.py
+++ scitopt/core/visualization.py
@@ -1,23 +1,24 @@
 import numpy as np
 import meshio
 import matplotlib.pyplot as plt
+import scitopt
 from scitopt.mesh import utils
 
 
 def save_info_on_mesh(
-    prb,
+    tsk,
     rho: np.ndarray,
     rho_prev: np.ndarray,
     file_path='levelset.vtk'
 ):
     
-    mesh = prb.mesh
-    dirichlet_ele = utils.get_elements_with_points(mesh, [prb.dirichlet_points])
-    F_ele = utils.get_elements_with_points(mesh, [prb.force_points])
+    mesh = tsk.mesh
+    dirichlet_ele = utils.get_elements_with_points(mesh, [tsk.dirichlet_points])
+    F_ele = utils.get_elements_with_points(mesh, [tsk.force_points])
     element_colors_df1 = np.zeros(mesh.nelements, dtype=int)
     element_colors_df2 = np.zeros(mesh.nelements, dtype=int)
-    element_colors_df1[prb.design_elements] = 1
-    element_colors_df1[prb.fixed_elements_in_rho] = 2
+    element_colors_df1[tsk.design_elements] = 1
+    element_colors_df1[tsk.fixed_elements_in_rho] = 2
     element_colors_df2[dirichlet_ele] = 1
     element_colors_df2[F_ele] = 2
     
@@ -42,19 +43,20 @@ def save_info_on_mesh(
 
 
 def export_submesh(
-    prb,
+    tsk: scitopt.mesh.TaskConfig,
     rho_projected: np.ndarray,
     threshold: float,
     dst_path: str
 ):
-    mesh = prb.mesh
-    remove_elements = prb.design_elements[rho_projected[prb.design_elements] <= threshold]
-    kept_elements = np.setdiff1d(prb.all_elements, remove_elements)
+    mesh = tsk.mesh
+    remove_elements = tsk.design_elements[rho_projected[tsk.design_elements] <= threshold]
+    kept_elements = scitopt.mesh.task.setdiff1d(tsk.all_elements, remove_elements)
     kept_t = mesh.t[:, kept_elements]
     unique_vertex_indices = np.unique(kept_t)
-    new_points = mesh.p[:, unique_vertex_indices]
+    new_points = np.ascontiguousarray(mesh.p[:, unique_vertex_indices])
     index_map = {old_idx: new_idx for new_idx, old_idx in enumerate(unique_vertex_indices)}
     new_elements = np.vectorize(index_map.get)(kept_t)
+    new_elements = np.ascontiguousarray(new_elements)
     meshtype = type(mesh)
     submesh = meshtype(new_points, new_elements)
     submesh.save(dst_path)
