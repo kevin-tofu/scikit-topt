f065975bcd563ce74ae5d91061385994714cc46d
Kevin-Tofu <koheitech001@gmail.com>
2025-04-05 09:01:09 +0900

    version update

--- scitopt/__init__.py
+++ scitopt/__init__.py
@@ -1,7 +1,8 @@
-__version__ = '0.0.3a'
-from scitopt import mesh, core, fea
+__version__ = '0.0.3b'
+from scitopt import mesh, core, fea, tools
 
 __all__ = []
 __all__.append("mesh")
 __all__.append("core")
 __all__.append("fea")
+__all__.append("tools")
