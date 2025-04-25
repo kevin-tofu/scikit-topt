bug fix in history/add scaling/amend calling dofs/change variable names

scitopt/core/optimizer/common.py
@@ -1,5 +1,5 @@
 import os
-from typing import Literal
+from typing import Literal, Optional
 import inspect
 import shutil
 import json
@@ -13,12 +13,14 @@ from scitopt.fea import solver
 from scitopt import filter
 from scitopt.fea import composer
 from scitopt.core import misc
+from scitopt.tools.logconf import mylogger
+logger = mylogger(__name__)
 
 
 
 @dataclass
 class SensitivityConfig():
-    dst_path: str = "./result"
+    dst_path: str = "./result/pytests"
     interpolation: Literal["SIMP", "RAMP"] = "SIMP"
     record_times: int=20
     max_iters: int=200
@@ -56,7 +58,8 @@ class SensitivityConfig():
     lambda_lower: float=1e-2
     lambda_upper: float=1e+2
     sensitivity_filter: bool = True
-    solver_option: str = "pyamg"
+    solver_option: str = "spsolve"
+    scaling: bool=False
 
 
     @classmethod
@@ -91,6 +94,9 @@ class SensitivityAnalysis():
     ):
         self.cfg = cfg
         self.tsk = tsk
+        if cfg.scaling is True:
+            self.scale()
+
         if not os.path.exists(self.cfg.dst_path):
             os.makedirs(self.cfg.dst_path)
         self.cfg.export(self.cfg.dst_path)
@@ -115,13 +121,37 @@ class SensitivityAnalysis():
         self.recorder.add("rho_projected", plot_type="min-max-mean-std")
         self.recorder.add("strain_energy", plot_type="min-max-mean-std")
         self.recorder.add("vol_error")
+        if isinstance(tsk.force, list):
+            self.recorder.add("u_max", plot_type="min-max-mean-std")
+        else:
+            self.recorder.add("u_max")
         self.recorder.add("compliance", ylog=True)
         self.recorder.add("scaling_rate", plot_type="min-max-mean-std")
         # self.recorder.add("dC", plot_type="min-max-mean-std")
         # self.recorder.add("lambda_v", ylog=False) # True
         self.schedulers = tools.Schedulers(self.cfg.dst_path)
-    
-    
+
+
+    def scale(self):
+        
+        bbox = np.ptp(self.tsk.mesh.p, axis=1)
+        L_max = np.max(bbox)
+        # L_mean = np.mean(bbox)
+        # L_geom = np.cbrt(np.prod(bbox))
+        self.L_scale = L_max
+        # self.tsk.mesh /= self.L_scale
+        self.F_scale = 10**5
+        self.tsk.scale(
+            1.0 / self.L_scale, 1.0 / self.F_scale
+        )
+
+
+    def unscale(self):
+        self.tsk.scale(
+            self.L_scale, self.F_scale
+        )
+
+
     def init_schedulers(self, export: bool=True):
 
         cfg = self.cfg
@@ -268,7 +298,7 @@ class SensitivityAnalysis():
             rho[tsk.force_elements] = 1.0
         else:
             rho[tsk.dirichlet_force_elements] = 1.0
-        rho[tsk.fixed_elements_in_rho] = 1.0
+        rho[tsk.fixed_elements] = 1.0
         self.init_schedulers()
         
         
@@ -279,7 +309,6 @@ class SensitivityAnalysis():
         grad_filtered = np.empty_like(rho)
         dC_drho_projected = np.empty_like(rho)
         strain_energy_ave = np.zeros_like(rho)
-        compliance_avg = np.zeros_like(rho)
         dH = np.zeros_like(rho)
 
         # dC_drho_ave = np.zeros_like(rho)
@@ -330,16 +359,19 @@ class SensitivityAnalysis():
             dC_drho_ave[:] = 0.0
             dC_drho_full[:] = 0.0
             strain_energy_ave[:] = 0.0
-            compliance_avg[:] = 0.0
+            compliance_avg = 0.0
+            u_max = list()
             for force in force_list:
                 dH[:] = 0.0
                 compliance, u = solver.compute_compliance_basis(
-                    tsk.basis, tsk.free_nodes, tsk.dirichlet_nodes, force,
+                    tsk.basis, tsk.free_dofs, tsk.dirichlet_dofs, force,
                     cfg.E0, cfg.E_min, p, tsk.nu,
                     rho_projected,
                     elem_func=density_interpolation,
                     solver=solver_option
                 )
+                
+                u_max.append(np.abs(u).max())
                 compliance_avg += compliance
                 strain_energy = composer.strain_energy_skfem(
                     tsk.basis, rho_projected, u,
@@ -416,6 +448,8 @@ class SensitivityAnalysis():
             self.recorder.feed_data("strain_energy", strain_energy_ave)
             self.recorder.feed_data("compliance", compliance_avg)
             self.recorder.feed_data("scaling_rate", scaling_rate)
+            u_max = u_max[0] if len(u_max) == 1 else np.ndarray(u_max)
+            self.recorder.feed_data("u_max", u_max)
 
             if iter % (cfg.max_iters // self.cfg.record_times) == 0 or iter == 1:
             # if True:
@@ -443,237 +477,8 @@ class SensitivityAnalysis():
                     # compliance=compliance
                 )
 
-        visualization.rho_histo_plot(
-            rho[tsk.design_elements],
-            f"{self.cfg.dst_path}/mesh_rho/last.jpg"
-        )
-        visualization.export_submesh(
-            tsk, rho, 0.5, f"{cfg.dst_path}/cubic_top.vtk"
-        )
-
-
-    def optimize_fosm(self):
-        tsk = self.tsk
-        cfg = self.cfg
-        if cfg.interpolation == "SIMP":
-        # if False:
-            # density_interpolation = composer.simp_interpolation_numba
-            density_interpolation = composer.simp_interpolation
-            dC_drho_func = derivatives.dC_drho_simp
-            val_init = 0.8
-        elif cfg.interpolation == "RAMP":
-            density_interpolation = composer.ramp_interpolation
-            dC_drho_func = derivatives.dC_drho_ramp
-            val_init = 0.4
-        else:
-            raise ValueError("should be SIMP/RAMP")
-        
-        elements_volume_design = tsk.elements_volume[tsk.design_elements]
-        elements_volume_design_sum = np.sum(elements_volume_design)
-
-        rho = np.zeros_like(tsk.all_elements, dtype=np.float64)
-        iter_begin = 1
-        if cfg.restart is True:
-            if cfg.restart_from > 0:
-                data = np.load(
-                    f"{cfg.dst_path}/data/{str(cfg.restart_from).zfill(6)}-rho.npz"
-                )
-            else:
-                iter, data_path = misc.find_latest_iter_file(f"{cfg.dst_path}/data")
-                data = np.load(data_path)
-                iter_begin = iter
-
-            rho[tsk.design_elements] = data["rho_design_elements"]
-            del data
-        else:
-            # _vol_frac = cfg.vol_frac if cfg.vol_frac_step < 0 else cfg.vol_frac_init
-            # rho += _vol_frac + 0.1 * (np.random.rand(len(tsk.all_elements)) - 0.5)
-            # rho += _vol_frac + 0.15
-            rho += val_init if cfg.vol_frac_step < 0 else cfg.vol_frac_init
-            np.clip(rho, cfg.rho_min, cfg.rho_max, out=rho)
-
-        if cfg.design_dirichlet is True:
-            rho[tsk.force_elements] = 1.0
-        else:
-            rho[tsk.dirichlet_force_elements] = 1.0
-        rho[tsk.fixed_elements_in_rho] = 1.0
-        self.init_schedulers()
-        
-        
-        rho_prev = np.zeros_like(rho)
-        rho_filtered = np.zeros_like(rho)
-        rho_projected = np.zeros_like(rho)
-        dH = np.empty_like(rho)
-        grad_filtered = np.empty_like(rho)
-        dC_drho_projected = np.empty_like(rho)
-        strain_energy_total = np.zeros_like(rho)
-        compliance_total = np.zeros_like(rho)
-        dH = np.zeros_like(rho)
-
-        # dC_drho_ave = np.zeros_like(rho)
-        dC_accum = np.zeros_like(rho)
-        dC_drho_ave = np.zeros_like(rho[tsk.design_elements])
-        scaling_rate = np.empty_like(rho[tsk.design_elements])
-        rho_candidate = np.empty_like(rho[tsk.design_elements])
-        tmp_lower = np.empty_like(rho[tsk.design_elements])
-        tmp_upper = np.empty_like(rho[tsk.design_elements])
-        force_list = tsk.force if isinstance(tsk.force, list) else [tsk.force]
-        filter_radius_prev = cfg.filter_radius_init if cfg.filter_radius_step > 0 else cfg.filter_radius
-        self.helmholz_solver.update_radius(tsk.mesh, filter_radius_prev)
-        for iter_loop, iter in enumerate(range(iter_begin, cfg.max_iters+iter_begin)):
-            print(f"iterations: {iter} / {cfg.max_iters}")
-            p, vol_frac, beta, move_limit, eta, percentile, filter_radius = (
-                self.schedulers.values(iter)[k] for k in [
-                    'p', 'vol_frac', 'beta', 'move_limit', 'eta', 'percentile', 'filter_radius'
-                ]
-            )
-            if filter_radius_prev != filter_radius:
-                print("Filter Update")
-                self.helmholz_solver.update_radius(tsk.mesh, filter_radius)
-            
-            print(f"p {p:.4f}, vol_frac {vol_frac:.4f}, beta {beta:.4f}, move_limit {move_limit:.4f}")
-            print(f"eta {eta:.4f}, percentile {percentile:.4f} filter_radius {filter_radius:.4f}")
-            rho_prev[:] = rho[:]
-            rho_filtered[:] = self.helmholz_solver.filter(rho)
-            projection.heaviside_projection_inplace(
-                rho_filtered, beta=beta, eta=cfg.beta_eta, out=rho_projected
-            )
-            dC_drho_ave[:] = 0.0
-            # solver_option = dict(solver="pyamg")
-            E0_nominal = cfg.E0
-            E_std_ratio = 0.1
-            E_list = [E0_nominal * (1 - E_std_ratio), E0_nominal, E0_nominal * (1 + E_std_ratio)]
-            # kappa = 1.0
-            kappa = 1.0e2
-
-            compliance_samples = []
-            strain_energy_samples = []
-            dC_drho_samples = []
-            for E_loop in E_list:
-                print(f"E_loop : {E_loop}")
-                compliance_total = 0.0
-                strain_energy_total[:] = 0.0
-                dC_accum[:] = 0.0
-
-                for force in force_list:
-                    dH[:] = 0.0
-                    compliance, u = solver.compute_compliance_basis(
-                        tsk.basis, tsk.free_nodes, tsk.dirichlet_nodes, force,
-                        E_loop, cfg.E_min, p, tsk.nu,
-                        rho_projected,
-                        elem_func=density_interpolation,
-                        solver_option=cfg.solver_option
-                    )
-                    compliance_total += compliance
-                    strain_energy = composer.strain_energy_skfem(
-                        tsk.basis, rho_projected, u,
-                        E_loop, cfg.E_min, p, tsk.nu,
-                        elem_func=density_interpolation
-                    )
-                    strain_energy_total += strain_energy
-                    
-                    np.copyto(
-                        dC_drho_projected,
-                        dC_drho_func(
-                            rho_projected,
-                            strain_energy, E_loop, cfg.E_min, p
-                        )
-                    )
-                    projection.heaviside_projection_derivative_inplace(
-                        rho_filtered,
-                        beta=beta, eta=cfg.beta_eta, out=dH
-                    )
-                    np.multiply(dC_drho_projected, dH, out=grad_filtered)
-                    dC_accum[:] += self.helmholz_solver.gradient(grad_filtered)
-                    
-                compliance_samples.append(compliance_total / len(force_list))
-                strain_energy_samples.append(strain_energy_total / len(force_list))
-                dC_drho_samples.append(dC_accum / len(force_list))
-
-
-            
-            compliance_avg = np.mean(compliance_samples)
-            compliance_std = np.std(compliance_samples)
-            robust_compliance = compliance_avg + kappa * compliance_std
-            print("Robust Compliance:", robust_compliance)
-
-            dC_drho_mean = np.mean(dC_drho_samples, axis=0)
-            dC_drho_std = np.std(dC_drho_samples, axis=0)
-            dC_accum[:] = dC_drho_mean + kappa * dC_drho_std
-
-            strain_energy_total[:] = np.mean(strain_energy_samples, axis=0)
-
-            if cfg.sensitivity_filter:
-                filtered = self.helmholz_solver.filter(dC_accum)
-                np.copyto(dC_accum, filtered)
-            
-            dC_drho_ave[:] = dC_accum[tsk.design_elements]
-            rho_candidate[:] = rho[tsk.design_elements] # Dont forget. inplace
-            
-            # 
-            self.rho_update(
-                # iter_loop,
-                iter,
-                rho_candidate,
-                rho_projected,
-                dC_drho_ave,
-                strain_energy_total,
-                scaling_rate,
-                move_limit,
-                eta,
-                beta,
-                tmp_lower,
-                tmp_upper,
-                percentile,
-                elements_volume_design,
-                elements_volume_design_sum,
-                vol_frac
-            )
-            # 
-            rho[tsk.design_elements] = rho_candidate
-            if cfg.design_dirichlet is True:
-                rho[tsk.force_elements] = 1.0
-            else:
-                rho[tsk.dirichlet_force_elements] = 1.0
-
-            filter_radius_prev = filter_radius
-            # rho_diff = np.mean(np.abs(rho[tsk.design_elements] - rho_prev[tsk.design_elements]))
-            print(
-                f"scaling_rate min/mean/max {scaling_rate.min()} {scaling_rate.mean()} {scaling_rate.max()}"
-            )
-            self.recorder.feed_data("rho", rho[tsk.design_elements])
-            self.recorder.feed_data("rho_projected", rho_projected[tsk.design_elements])
-            self.recorder.feed_data("strain_energy", strain_energy_total)
-            self.recorder.feed_data("compliance", robust_compliance)
-            self.recorder.feed_data("scaling_rate", scaling_rate)
-            
-            
-            
-            if iter % (cfg.max_iters // self.cfg.record_times) == 0 or iter == 1:
-            # if True:
-                print(f"Saving at iteration {iter}")
-                self.recorder.print()
-                # self.recorder_params.print()
-                self.recorder.export_progress()
-                
-                visualization.save_info_on_mesh(
-                    tsk,
-                    rho_projected, rho_prev, strain_energy_total,
-                    cfg.vtu_path(iter),
-                    cfg.image_path(iter, "rho"),
-                    f"Iteration : {iter}",
-                    cfg.image_path(iter, "strain_energy"),
-                    f"Iteration : {iter}"
-                )
-                visualization.export_submesh(
-                    tsk, rho, 0.5, f"{cfg.dst_path}/cubic_top.vtk"
-                )
-                np.savez_compressed(
-                    f"{cfg.dst_path}/data/{str(iter).zfill(6)}-rho.npz",
-                    rho_design_elements=rho[tsk.design_elements],
-                    # compliance=compliance
-                )
-
+        if cfg.scaling is True:
+            self.unscale()
         visualization.rho_histo_plot(
             rho[tsk.design_elements],
             f"{self.cfg.dst_path}/mesh_rho/last.jpg"

scitopt/fea/composer.py
@@ -702,7 +702,7 @@ def compute_element_stress_tensor(w):
     return stress
 
 
-def stress_skfem(
+def stress_tensor_skfem(
     basis: skfem.Basis,
     rho: np.ndarray, u, 
     E0: float, Emin: float, p: float, nu: float,
@@ -723,6 +723,7 @@ def stress_skfem(
         lam_elem=lam_elem, mu_elem=mu_elem
     )
     return stress_tensor  # shape: (n_elem, n_qp, 3, 3)
+
 
 
 def von_mises_from_stress_tensor(stress_tensor: np.ndarray) -> np.ndarray:
@@ -1073,7 +1074,7 @@ if __name__ == '__main__':
         # print("The Strain Energy Each =", elem_energy_simp)
         print("Difference =", np.sum((elem_energy_simp - element_U)**2))
 
-        stress = stress_skfem(
+        stress = stress_tensor_skfem(
             tsk.basis, np.ones(tsk.mesh.nelements), u,
             1.0, 0.0, 1.0, 0.3
         )

scitopt/fea/solver.py
@@ -8,7 +8,7 @@ from scitopt.fea import composer
 
 
 def compute_compliance_simp_basis(
-    basis, free_nodes, dirichlet_nodes, force,
+    basis, free_dofs, dirichlet_dofs, force,
     E0, Emin, p, nu0,
     rho,
 ) -> tuple:
@@ -16,11 +16,11 @@ def compute_compliance_simp_basis(
         basis, rho, E0,
         Emin, p, nu0
     )
-    K_e, F_e = skfem.enforce(K, force, D=dirichlet_nodes)
+    K_e, F_e = skfem.enforce(K, force, D=dirichlet_dofs)
     # u = scipy.sparse.linalg.spsolve(K_e, F_e)
     u = skfem.solve(K_e, F_e)
-    f_free = force[free_nodes]
-    compliance = f_free @ u[free_nodes]
+    f_free = force[free_dofs]
+    compliance = f_free @ u[free_dofs]
     return (compliance, u)
 
 
@@ -59,7 +59,7 @@ def solve_u(
 
 
 def compute_compliance_basis(
-    basis, free_nodes, dirichlet_nodes, force,
+    basis, free_dofs, dirichlet_dofs, force,
     E0, Emin, p, nu0,
     rho,
     elem_func: Callable = composer.simp_interpolation,
@@ -88,27 +88,28 @@ def compute_compliance_basis(
     _maxiter = min(1000, max(300, n_dof // 5)) if maxiter is None else maxiter
     
     
-    # K_e, F_e = skfem.enforce(K, force, D=dirichlet_nodes)
+    # K_e, F_e = skfem.enforce(K, force, D=dirichlet_dofs)
     K_csr = K.tocsr()
-    K_c, F_c, U_c, I = skfem.condense(K_csr, force, D=dirichlet_nodes)
+    K_c, F_c, U_c, I = skfem.condense(K_csr, force, D=dirichlet_dofs)
     U_c[I] = solve_u(
         K_c, F_c, chosen_solver=chosen_solver,
         rtol=rtol, maxiter=_maxiter
     )
-    U_c[dirichlet_nodes] = 0.0
+    U_c[dirichlet_dofs] = 0.0
     u = U_c
-    # K_e, F_e = skfem.enforce(K_csr, force, D=dirichlet_nodes)
+    # K_e, F_e = skfem.enforce(K_csr, force, D=dirichlet_dofs)
     # u = solve_u(
     #     K_e, F_e , chosen_solver=chosen_solver,
     #     rtol=rtol, maxiter=_maxiter
     # )
-    f_free = force[free_nodes]
-    compliance = f_free @ u[free_nodes]
-    return (compliance, u)
+    f_free = force[free_dofs]
+    compliance = f_free @ u[free_dofs]
+    
+    return (float(compliance), u)
 
 
 def compute_compliance_basis_numba(
-    basis, free_nodes, dirichlet_nodes, force,
+    basis, free_dofs, dirichlet_dofs, force,
     E0, Emin, p, nu0,
     rho,
     elem_func: Callable = composer.simp_interpolation_numba,
@@ -119,7 +120,7 @@ def compute_compliance_basis_numba(
     K = composer.assemble_stiffness_matrix_numba(
         basis, rho, E0, Emin, p, nu0, elem_func
     )
-    K_e, F_e = skfem.enforce(K, force, D=dirichlet_nodes)
+    K_e, F_e = skfem.enforce(K, force, D=dirichlet_dofs)
     n_dof = K.shape[0]
 
     # Solver auto-selection
@@ -142,13 +143,13 @@ def compute_compliance_basis_numba(
         rtol=rtol, maxiter=_maxiter
     )
 
-    f_free = force[free_nodes]
-    compliance = f_free @ u[free_nodes]
+    f_free = force[free_dofs]
+    compliance = f_free @ u[free_dofs]
     return (compliance, u)
 
 
 def compute_compliance_batch(
-    basis, free_nodes, dirichlet_nodes, force_batch,
+    basis, free_dofs, dirichlet_dofs, force_batch,
     E0, Emin, p, nu0,
     rho,
     elem_func: Callable = composer.ramp_interpolation_numba,
@@ -170,13 +171,13 @@ def compute_compliance_batch(
     n_cases = force_batch.shape[1]
     n_dof = K.shape[0]
     
-    # Apply boundary condition once (assuming same dirichlet_nodes for all)
-    K_e, _ = skfem.enforce(K, np.zeros(n_dof), D=dirichlet_nodes)
+    # Apply boundary condition once (assuming same dirichlet_dofs for all)
+    K_e, _ = skfem.enforce(K, np.zeros(n_dof), D=dirichlet_dofs)
     
     # Apply boundary condition to all force vectors
     F_batch = np.zeros((n_dof, n_cases))
     for i in range(n_cases):
-        _, F_batch[:, i] = skfem.enforce(K, force_batch[:, i], D=dirichlet_nodes)
+        _, F_batch[:, i] = skfem.enforce(K, force_batch[:, i], D=dirichlet_dofs)
 
     if solver != "spsolve":
         raise NotImplementedError("Currently only 'spsolve' is supported for batch.")
@@ -184,22 +185,11 @@ def compute_compliance_batch(
     # Solve Ku = f for all cases
     u_all = scipy.sparse.linalg.spsolve(K_e, F_batch)  # shape: (n_dof, n_cases)
 
-    f_free = force_batch[free_nodes, :]
-    u_free = u_all[free_nodes, :]
+    f_free = force_batch[free_dofs, :]
+    u_free = u_all[free_dofs, :]
     compliance_all = np.sum(f_free * u_free, axis=0)  # f^T u for each case
 
     return compliance_all, u_all
-
-
-def compute_compliance_simp(
-    prb,
-    rho,
-    p
-) -> tuple:
-    return compute_compliance_simp_basis(
-        prb.basis, prb.free_nodes, prb.dirichlet_nodes, prb.force,
-        prb.E0, prb.Emin, p, prb.nu0, rho
-    )
 
 
 if __name__ == '__main__':
@@ -210,7 +200,7 @@ if __name__ == '__main__':
     rho = np.ones(tsk.all_elements.shape)
     p = 1.0
     compliacne, u = compute_compliance_basis_numba(
-        tsk.basis, tsk.free_nodes, tsk.dirichlet_nodes, tsk.force,
+        tsk.basis, tsk.free_dofs, tsk.dirichlet_dofs, tsk.force,
         tsk.E0, tsk.Emin, p, tsk.nu0,
         rho,
     )

scitopt/mesh/toy_problem.py
@@ -82,15 +82,11 @@ def toy_base(
     dirichlet_points = utils.get_point_indices_in_range(
         basis, (0.0, 0.03), (0.0, y_len), (0.0, z_len)
     )
-    dirichlet_nodes = utils.get_dofs_in_range(
-        basis, (0.0, 0.03), (0.0, y_len), (0.0, z_len)
-    ).all()
+    dirichlet_nodes = basis.get_dofs(dirichlet_points).all()
     F_points = utils.get_point_indices_in_range(
         basis, (x_len - eps, x_len+0.1), (y_len*2/5, y_len*3/5), (z_len*2/5, z_len*3/5)
     )
-    F_nodes = utils.get_dofs_in_range(
-        basis, (x_len - eps, x_len+0.1), (y_len*2/5, y_len*3/5), (z_len*2/5, z_len*3/5)
-    ).nodal['u^1']
+    F_nodes = basis.get_dofs(nodes=F_points).nodal['u^1']
     design_elements = utils.get_elements_in_box(
         mesh,
         # (0.3, 0.7), (0.0, 1.0), (0.0, 1.0)
@@ -103,7 +99,6 @@ def toy_base(
     return task.TaskConfig.from_defaults(
         E0,
         0.30,
-        mesh,
         basis,
         dirichlet_points,
         dirichlet_nodes,
@@ -139,15 +134,13 @@ def toy2():
     dirichlet_points = utils.get_point_indices_in_range(
         basis, (0.0, 0.03), (0.0, y_len), (0.0, z_len)
     )
-    dirichlet_nodes = utils.get_dofs_in_range(
-        basis, (0.0, 0.03), (0.0, y_len), (0.0, z_len)
-    ).all()
+    dirichlet_nodes = basis.get_dofs(dirichlet_points).all()
     F_points = utils.get_point_indices_in_range(
         basis, (x_len, x_len), (y_len*2/5, y_len*3/5), (z_len*2/5, z_len*3/5)
     )
-    F_nodes = utils.get_dofs_in_range(
-        basis, (x_len, x_len), (y_len*2/5, y_len*3/5), (z_len*2/5, z_len*3/5)
-    ).nodal['u^1']
+    F_nodes = basis.get_dofs(nodes=F_points).nodal["u^1"]
+    print(F_nodes)
+    print(F_nodes.shape)
     design_elements = utils.get_elements_in_box(
         mesh,
         # (0.3, 0.7), (0.0, 1.0), (0.0, 1.0)
@@ -155,14 +148,12 @@ def toy2():
     )
 
     print("generate config")
-    E0 = 1.0
+    E0 = 210e9
     F = [0.3, -0.3]
     print("F:", F)
     return task.TaskConfig.from_defaults(
         E0,
         0.30,
-        1e-3 * E0,
-        mesh,
         basis,
         dirichlet_points,
         dirichlet_nodes,
@@ -201,7 +192,7 @@ def toy_msh(
         z_len = 0.5
     elif task_name == "pull_2":
         # x_len = 8.0
-        x_len = 6.0
+        x_len = 4.0
         # x_len = 6.0
         y_len = 3.0
         z_len = 0.5
@@ -217,15 +208,6 @@ def toy_msh(
     # eps = np.min(dists) * 1.2
     # eps = np.min(dists) * 5.0
     print(f"eps: {eps}")
-
-    # Check Index Order.
-    # print("Before mesh.t fix:", mesh.t[:, 0])
-    # t_fixed = utils.fix_tetrahedron_orientation(mesh.t, mesh.p)
-    # print("After fix        :", t_fixed[:, 0])
-    # mesh_fixed = MeshTet(mesh.p, t_fixed)
-    # print("Mesh fixed .t    :", mesh_fixed.t[:, 0])
-    # composer._get_elements_volume(mesh_fixed.t, mesh_fixed.p)
-    
     # mesh = skfem.MeshTet.from_mesh(meshio.read(msh_path))
     if isinstance(mesh, skfem.MeshTet):
         e = skfem.ElementVector(skfem.ElementTetP1())
@@ -240,9 +222,7 @@ def toy_msh(
     dirichlet_points = utils.get_point_indices_in_range(
         basis, (0.0, 0.05), (0.0, y_len), (0.0, z_len)
     )
-    dirichlet_nodes = utils.get_dofs_in_range(
-        basis, (0.0, 0.0), (0.0, y_len), (0.0, z_len)
-    ).all()
+    dirichlet_nodes = basis.get_dofs(nodes=dirichlet_points).all()
     
     if task_name == "down":
         F_points = utils.get_point_indices_in_range(
@@ -252,16 +232,8 @@ def toy_msh(
             # (y_len*2/5, y_len*3/5),
             # (z_len*2/5, z_len*3/5)
         )
-        F_nodes = utils.get_dofs_in_range(
-            basis,
-            (x_len, x_len+0.0),
-            # (x_len-eps, x_len+0.05),
-            (0, y_len),
-            (0.0, eps)
-            # (y_len*2/5, y_len*3/5),
-            # (z_len*2/5, z_len*3/5)
-        ).nodal['u^3']
-        F = -0.002
+        F_nodes = basis.get_dofs(nodes=F_points).nodal["u^2"]
+        F = -100
     elif task_name == "pull":
         F_points = utils.get_point_indices_in_range(
             basis,
@@ -269,34 +241,17 @@ def toy_msh(
             (y_len*2/5, y_len*3/5),
             (z_len*2/5, z_len*3/5)
         )
-        F_nodes = utils.get_dofs_in_range(
-            basis,
-            (x_len-eps, x_len+0.05),
-            (y_len*2/5, y_len*3/5),
-            (z_len*2/5, z_len*3/5)
-        ).nodal['u^1']
-        # F = 0.0001
-        F = 0.001
-        
-        print("F_points:", F_points)
-        print("F_nodes:", F_nodes)
+        F_nodes = basis.get_dofs(nodes=F_points).nodal["u^1"]
+        F = 1200.0
     elif task_name == "pull_2":
-        # eps = eps * 4
-        # eps = 1.8
-        # print(f"eps: {eps}")
         F_points = utils.get_point_indices_in_range(
             basis,
             (x_len-eps, x_len+0.05),
             (y_len*2/5, y_len*3/5),
             (z_len*2/5, z_len*3/5)
         )
-        F_nodes = utils.get_dofs_in_range(
-            basis,
-            (x_len-eps, x_len+0.05),
-            (y_len*2/5, y_len*3/5),
-            (z_len*2/5, z_len*3/5)
-        ).nodal['u^1']
-        F = 0.0002
+        F_nodes = basis.get_dofs(nodes=F_points).nodal["u^1"]
+        F = 100
     
     design_elements = utils.get_elements_in_box(
         mesh,
@@ -306,7 +261,7 @@ def toy_msh(
     
     p = basis.mesh.p
     print("generate config")
-    E0 = 1.0
+    E0 = 210e9
     print("F:", F)
     print("F_points:", F_points.shape)
     print("F_nodes:", F_nodes.shape)
@@ -314,7 +269,6 @@ def toy_msh(
     return task.TaskConfig.from_defaults(
         E0,
         0.30,
-        mesh,
         basis,
         dirichlet_points,
         dirichlet_nodes,