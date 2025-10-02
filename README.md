cfbe454ab6f027aa5e39920cd25b539d666e6234
Kevin-Tofu <koheitech001@gmail.com>
2025-10-03 01:24:51 +0900

    update version name

--- README.md
+++ README.md
@@ -196,7 +196,7 @@ If you use Scikit Topt in your research or software, please cite it as:
   publisher    = {Zenodo},
   doi          = {10.5281/zenodo.15441499},
   url          = {https://doi.org/10.5281/zenodo.15441499},
-  note         = {Version 0.2.9}
+  note         = {Version 0.2.10}
 }
 ```
 
--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../scikit-topt'))
 project = 'scikit-topt'
 copyright = '2025, Kohei Watanabe'
 author = 'Kohei Watanabe'
-release = '0.2.9'
+release = '0.2.10'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
